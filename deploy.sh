764af6779aaa23c86c58bf3cf9baacc07e56e96b
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2019-03-05 08:53:21 +0100

    Renaming operator to service

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ GITHUB_ORG=$2
 GITHUB_USER=$3
 GITHUB_API_TOKEN=$4
 
-# Deploy operator
-rm -f config/gen/operator.yaml
+# Deploy service
+rm -f config/gen/service.yaml
 
-cat config/operator.yaml | \
-  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/operator.yaml
+cat config/service.yaml | \
+  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/service.yaml
 
-kubectl apply -f config/gen/operator.yaml
+kubectl apply -f config/gen/service.yaml
 
 # Create secret for USER and API_TOKEN
 kubectl create secret generic -n keptn github-credentials --from-literal=gitorg="$GITHUB_ORG" --from-literal=gituser="$GITHUB_USER" --from-literal=gittoken="$GITHUB_API_TOKEN"
