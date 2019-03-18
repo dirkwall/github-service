6675e7aa546217d643618f71fe060e8139669117
Florian Bacher <florian.bacher@dynatrace.com>
2019-03-18 12:31:56 +0100

    ignore not-found

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ rm -f config/gen/service.yaml
 cat config/service.yaml | \
   sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/service.yaml
 
-kubectl delete -f config/gen/service.yaml
+kubectl delete -f config/gen/service.yaml --ignore-not-found
 kubectl apply -f config/gen/service.yaml
