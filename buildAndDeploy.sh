1d9a1398a9166f0333ce5c361ce88bf18ce89ee4
Florian Bacher <bacherfl@gmail.com>
2019-04-05 08:23:27 +0200

    fixed buildAndDeploy

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -5,10 +5,10 @@ REGISTRY_URI=$(kubectl describe svc docker-registry -n keptn | grep IP: | sed 's
 kubectl create secret generic -n keptn github-credentials --from-literal=org=githuborg --from-literal=user=githubuser --from-literal=token=token
 
 # Deploy service
-rm -f config/gen/service.yaml
+rm -f config/gen/service-build.yaml
 
-cat config/service.yaml | \
-  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/service.yaml
+cat config/service-build.yaml | \
+  sed 's~REGISTRY_URI_PLACEHOLDER~'"$REGISTRY_URI"'~' >> config/gen/service-build.yaml
 
-kubectl delete -f config/gen/service.yaml --ignore-not-found
-kubectl apply -f config/gen/service.yaml
+kubectl delete -f config/gen/service-build.yaml --ignore-not-found
+kubectl apply -f config/gen/service-build.yaml
